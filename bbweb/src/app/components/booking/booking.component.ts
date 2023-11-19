a18d259cdfb087d214e03d4856ccedccad41b47c
imad0123456789 <95219016+imad0123456789@users.noreply.github.com>
2023-11-19 11:08:31 +0100

    20

--- bbweb/src/app/components/booking/booking.component.ts
+++ bbweb/src/app/components/booking/booking.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { BookingService } from '../booking.service';
+import {BookingInterface} from "../../interfaces/booking.interface";
+import {WeekDay} from "@angular/common";
 
 @Component({
   selector: 'app-booking',
@@ -7,7 +9,20 @@ import { BookingService } from '../booking.service';
   styleUrls: ['./booking.component.css']
 })
 export class BookingComponent {
-  booking = { name: 'Imad', date: '25-12-2023', time: '10:00' };
+
+  booking =
+    {
+      Date: Date,
+      day: 'Friday',
+      Time: '10:30',
+      DoctorCategory : 'botox',
+      doctorId: 'ok2pEeFZyosKRka30AY2',
+      doctorName: 'Kassem Mannaki',
+      status: 'upcoming',
+      userId: 'qvJLNUwNh1j2EbYGMMr7',
+    }
+
+  //booking = { name: 'Imad', date: '25-12-2023', time: '10:00' };
 
   constructor(private bookingService: BookingService) {}
 
---
+++ bbweb/src/app/interfaces/booking.interface.ts
@@ -0,0 +1,11 @@
+
+export interface BookingInterface {
+  Date?: Date,
+  day?: string,
+  Time?: string,
+  DoctorCategory? : string,
+  doctorId?: string,
+  doctorName?: string,
+  status?: string,
+  userId: string,
+}
